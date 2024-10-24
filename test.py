bdadd26afcaf4c833deadcce812b1d3f1217b187
Fra-Maraglino <francesco.maraglino@edu.unife.it>
2024-10-24 17:53:44 +0200

    Ultimating simulation and adding "realtime" median of packets in  and  packets in the system

--- test.py
+++ test.py
@@ -7,6 +7,7 @@ from packet import Packet
 y = 50
 mu = 2
 c = 20
+
 queue = asyncio.Queue()
 packet_array = []
 
@@ -45,8 +46,7 @@ async def packet_creator(y):
 
 #Server that process packet with rate y
 async def server(mu, server_id):
-    while True:
-        
+    while True:       
         #get packet from queue
         packet = await queue.get()
         packet.setQueueTime(time.time())
@@ -58,25 +58,54 @@ async def server(mu, server_id):
         packet_array_t.pop()
         
 
+async def getMediumPacket():
+    while True:
+        await asyncio.sleep(1)
+        n_packet_sistem.append(len(packet_array_t))
+        n_packet_queue_t.append(queue.qsize())
+
+
 async def main():
     #start producer and server
     sim_time = 10
     packet_creator_task = asyncio.create_task(packet_creator(y))
     server_task = [asyncio.create_task(server(mu, i+1)) for i in range(c)]
+    medium_packet_task = asyncio.create_task(getMediumPacket())
     
     #time of simulation
     await asyncio.sleep(sim_time)
+
     #cancel producer and server
     packet_creator_task.cancel()
     queuesize = queue.qsize()
+    medium_packet_task.cancel()
+
     for task in server_task:
         task.cancel()
-    medium_packet = (len(packet_array)+queuesize)/sim_time
-    mediumqueue = queuesize/sim_time
+
+
+    #calculate medium number of packet in the queue
+    queue_tot = 0
+    for n in n_packet_queue_t:
+        queue_tot += n
+    mediumqueue = queue_tot/len(n_packet_queue_t)
+
+    #calculate medium number of packet in the system
+    packet_tot = 0
+    for n in n_packet_sistem:
+        packet_tot += n
+    medium_packet = packet_tot / len(n_packet_sistem)
+    
     print("Simulation ended with ", len(packet_array), " packets processed and there are ", queuesize, " packets in queue")
     print("The average time a packet spends in the system is: ", getMedianTime(packet_array))
     print("The average time a packet spends in the queue is: ", getMediumQueueTime(packet_array))
+    
+    print("\n")
+    print("The average number of packets in the queue is ", mediumqueue)
     print("The average number of packets in the system is ", medium_packet)
+
+
+
 asyncio.run(main())
 
 
