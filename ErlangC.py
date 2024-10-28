be3ab35cbb33d64a7a1766eb9f329109a5a44b4b
Chris3444 <94828434+Chris3444@users.noreply.github.com>
2024-10-28 16:43:42 +0100

    Update ErlangC.py

--- ErlangC.py
+++ ErlangC.py
@@ -3,12 +3,12 @@ import math
 # implementazione della C di Erlang
 def ErlangC(c, A): 
     intermezzo = (c / (c - A)) #valore parziale per il calcolo di numeratore e denominatore
-    numeratore = (pow(A, c) / math.factorial(c)) * intermezzo
+    numeratore = (math.pow(A, c) / math.factorial(c)) * intermezzo
     
     risultato_sommatoria = 0
     for k in range(0, c - 1):
-        risultato_sommatoria += (pow(A,k) / math.factorial(k)) 
+        risultato_sommatoria += (math.pow(A,k) / math.factorial(k)) 
 
-    denominatore = risultato_sommatoria + ((pow(A,c) / math.factorial(c)) * intermezzo)
+    denominatore = risultato_sommatoria + ((math.pow(A,c) / math.factorial(c)) * intermezzo)
 
     return (numeratore / denominatore)
